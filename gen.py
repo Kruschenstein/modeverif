13ef6dabd8e0fde6b3acab24814897c33247bdee
Fabre Thomas <mas.tho3@yahoo.fr>
2016-12-13 21:42:57 +0100

    Finalisation du nprocess generation + LTL

--- gen.py
+++ gen.py
@@ -4,37 +4,52 @@ from sys import argv
 
 N = int(argv[1]) if len(argv) >= 2 else 2
 
-def gen_processus():
+def capitalize(word):
+    """
+    return the word with first letter as capital letter
+    """
+    return word[0].upper() + word[1:]
+
+def gen_processus(nb_process):
     """
     generate Processus automaton
     """
-    return """Processus = [setQ, setTURN, wait, critic, end]
-{
-    etat = 5;
-    init = 0;
+    processus = "Processus = [%s, critic, end]\n{\n" % ", ".join([
+        "setQ%i, setTURN%i, wait%i" % (i, i, i) for i in range(1, nb_process)
+    ])
+    processus += INDENTATION + "etat = %i;\n" % (3 * (nb_process -1) + 2)
+    processus += INDENTATION + "init = 0;\n\n"
 
-    0 = setQ;
-    1 = setTURN;
-    2 = wait;
-    3 = critic;
-    4 = end;
+    for_statement = ["setQ%i", "setTURN%i", "wait%i"]
+    states = []
+    for i in range(1, nb_process):
+        for statement in for_statement:
+            states.append(statement % i)
 
-    0 -> 1 [goSetTURN];
-    0 -> 4 [goEnd];
-    1 -> 2 [goWait];
-    2 -> 3 [goCritic];
-    2 -> 0 [goSetQ];
-    3 -> 0 [goSetQ];
-};;
+    states.append("critic")
+    states.append("end")
+    for i in range(len(states)):
+        processus += INDENTATION + "%i = %s;\n" % (i, states[i])
+    processus += "\n"
 
-"""
+    for i in range(len(states) -2):
+        processus += INDENTATION + "%i -> %i [go%s];\n" % (i, i +1,
+                                                           capitalize(
+                                                               states[i+1]))
+
+    processus += INDENTATION + "%i -> %i [go%s];\n" % (len(states) -2, 0,
+                                                       capitalize(states[0]))
+    processus += INDENTATION + "%i -> %i [go%s];\n" % (0, len(states) -1,
+                                                       capitalize(states[-1]))
+
+    processus += "};;"
+    return processus
 
 INDENTATION = " " * 3
 
 def gen_system_header(nb_process):
     """
-    generate system = <Processus P1, ..., VarQ Q1, .., VarTURN TURN1, ..,
-                       VarJ J>
+    generate system = <Processus P1, ..., VarQ Q1, .., VarTURN TURN1, ..>
     """
     system = "system = <"
     for i in range(1, nb_process +1):
@@ -46,7 +61,7 @@ def gen_system_header(nb_process):
     for i in range(1, nb_process):
         system += "VarTURN TURN%i, " % i
 
-    system += "VarJ J>\n"
+    system = system[:-2] + ">\n"
     return system
 
 def gen_go_end(nb_process):
@@ -61,7 +76,7 @@ def gen_go_end(nb_process):
         system += ", ".join(tab)
         system += ", "
         system += ", ".join(["_"] * (nb_process + nb_process -1))
-        system += ", _> -> p%i_end;\n" % (i +1)
+        system += "> -> p%i_end;\n" % (i +1)
     return system
 
 def gen_qi_set(nb_process, j):
@@ -72,14 +87,15 @@ def gen_qi_set(nb_process, j):
     for i in range(1, nb_process+1):
         system += INDENTATION + "<"
         system += "_, " * (i -1)
-        system += "goSetTURN, "
+        system += "goSetTURN%i, " % j
         system += "_, " * (nb_process - i)
         system += "_, " * (i -1)
         system += "set%i, " % j
         system += "_, " * (nb_process -i)
         for _ in range(1, nb_process):
             system += "_, "
-        system += "is%i> -> p%i_set_Q%i_%i;\n" % (j, i, i, j)
+        system = system[:-2]
+        system += "> -> p%i_set_Q%i_%i;\n" % (i, i, j)
     return system
 
 def gen_turnj_set(nb_process, j):
@@ -90,12 +106,12 @@ def gen_turnj_set(nb_process, j):
     for i in range(1, nb_process+1):
         system += INDENTATION + "<"
         system += "_, " * (i -1)
-        system += "goWait, "
+        system += "goWait%i, " % j
         system += "_, " * (nb_process - i + nb_process)
-        system += "_," * (j -1)
+        system += "_, " * (j -1)
         system += "set%i" % i
         system += ", _" * (nb_process -j -1)
-        system += ", is%i> -> p%i_set_TURN%i_%i;\n" % (j, i, j, i)
+        system += "> -> p%i_set_TURN%i_%i;\n" % (i, j, i)
     return system
 
 def gen_system(nb_process):
@@ -124,38 +140,43 @@ def gen_system(nb_process):
         system += "\n"
 
         # wait until (Vk != i => Q[k] < j) or TURN[j] != i
-        for i in range(1, nb_process+1):
+
+        for i in range(1, N+1):
             system += INDENTATION + "<"
             system += "_, " * (i -1)
-            system += "goCritic, " if j == nb_process -1 else "goSetQ, "
-            system += "_, " * (nb_process * 2- i)
+            system += "goCritic, " if j == N -1 else "goSetQ%i, " % (j +1)
+            system += "_, " * (N - i)
+            var_q = ["_"] * N
+            # var_q[i -1] = "is%i" % j
+            system += ", ".join(var_q) + ", "
             system += "_, " * (j -1)
             system += "isNe%i" % i
-            system += ", _" * (nb_process -j -1)
-            system += ", is%i> -> p%i_unlock_iteration_%i;\n" % (j, i, j)
-
+            system += ", _" * (N -j -1)
+            system += "> -> p%i_unlock_iteration_%i;\n" % (i, j)
             system += INDENTATION + "<"
             system += "_, " * (i -1)
-            system += "goCritic, " if j == nb_process -1 else "goSetQ, "
-            system += "_, " * (nb_process - i)
-            lower_than = ["isLt%i" % j] * nb_process
+            system += "goCritic, " if j == N -1 else "goSetQ%i, " % (j +1)
+            system += "_, " * (N - i)
+            lower_than = ["isLt%i" % j] * N
             lower_than[i -1] = "_"
             system += ", ".join(lower_than) + ", "
-            for _ in range(1, nb_process):
+            for _ in range(1, N):
                 system += "_, "
-            system += "is%i> -> p%i_unlock_iteration_%i;\n" % (j, i, j)
+            system = system[:-2]
+            system += "> -> p%i_unlock_iteration_%i;\n" % (i, j)
         system += "\n"
 
     # Q[i] := 0
     for i in range(1, nb_process+1):
         system += INDENTATION + "<"
         system += "_, " * (i -1)
-        system += "goSetQ, "
+        system += "goSetQ1, "
         system += "_, " * (nb_process - i)
         system += "_, " * (i -1)
         system += "set0, "
         system += "_, " * (nb_process - i + nb_process -1)
-        system += "_> -> p%i_set_Q%i_0;\n" % (i, i)
+        system = system[:-2]
+        system += "> -> p%i_set_Q%i_0;\n" % (i, i)
 
     system += "};;\n"
     return system
@@ -234,7 +255,7 @@ def gen_bug_verif(nb_process):
     generate CTL bug path
     """
     bugs = "bug1 = system -> AX(false) && (%s);;\n" % " || ".join([
-        "P%i.wait" % i for i in range(1, nb_process+1)])
+        "P%i.wait%i" % (i, nb_process -1) for i in range(1, nb_process+1)])
 
     critics = []
     for i in range(1, nb_process):
@@ -245,9 +266,33 @@ def gen_bug_verif(nb_process):
 todot dot/nprocess_bug2.dot bug2;;"""
     return bugs
 
-PROG = "%s\n%s\n%s\n%s\n%s\n%s\n" % (gen_var_j(N), gen_var_q(N),
-                                     gen_var_turn(N), gen_processus(),
-                                     gen_system(N), gen_bug_verif(N))
+def gen_ltl(nb_process):
+    """
+    generate ltl automaton
+    """
+    ltl = "load ltl_sys.mso;;\n"
+    ltl += "S = automaton system;;\n\n"
+
+    critics = []
+    for i in range(1, nb_process):
+        for j in range(i +1, nb_process+1):
+            critics.append("(P%i.critic && P%i.critic)" % (i, j))
+
+    ltl += "em = S && exclusion_mutuelle[p <- %s];;\n" % " || ".join(critics)
+    ltl += "exmut = reduce em;;\n\n"
+
+    ltl += "ef = S && equite_forte[a <- P1.wait%i, b <- P1.critic];;\n" % (
+        nb_process -1)
+    ltl += "eqfort = reduce ef;;\n\n"
+
+    ltl += """todot dot/nprocess_ltl_eqfort.dot eqfort;;
+todot dot/nprocess_ltl_exmut.dot exmut;;"""
+
+    return ltl
+
+PROG = "%s\n%s\n%s\n%s\n%s\n%s\n" % (gen_var_q(N), gen_var_turn(N),
+                                     gen_processus(N), gen_system(N),
+                                     gen_bug_verif(N), gen_ltl(N))
 print(PROG)
 
 with open("sys.mso", "w") as f:
